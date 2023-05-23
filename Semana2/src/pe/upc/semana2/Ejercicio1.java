4208a5fc7f808cec328d75aac8ec99e07a379c40
professor <pciscflo@upc.edu.pe>
2023-05-22 19:44:46 -0500

    02.00

---
+++ Semana2/src/pe/upc/semana2/Ejercicio1.java
@@ -0,0 +1,20 @@
+package pe.upc.semana2;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        /*
+        Varios comentarios
+        a la vez
+         */
+        String nombre = "Juan Torres";
+        String codigo ="A2001";
+        int edad = 19;
+        //declarando reales
+        double precio = 2.45; //redondeo
+        byte n = 0;
+        System.out.println("Su nombre es " + nombre + " y su edad es "+ edad);
+        System.out.printf("El precio de la gaseosa es: %.1f \n", precio);
+        System.out.println("Fin.");
+
+    }
+}
