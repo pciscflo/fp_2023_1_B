99a509dc4ffc0e75347166a8d5c7460b90ff8a48
professor <pciscflo@upc.edu.pe>
2023-05-29 21:50:32 -0500

    03.00

--- Semana3/src/pe/upc/selectivas/Ejercicio8.java
+++ Semana3/src/pe/upc/selectivas/Ejercicio8.java
@@ -2,13 +2,36 @@ package pe.upc.selectivas;
 
 public class Ejercicio8 {
     public static void main(String[] args) {
+        System.out.println("Jugador 1");
         System.out.println(calcularSueldo(26, "E"));
+        //imprimir el sueldo final y su bono
+        System.out.println("-------------------------------");
+        System.out.println("Jugador 2");
+        System.out.println(calcularSueldo(29,"E"));
+        System.out.println("Su bono es: " + calcularBono("E"));
     }
     static double calcularSueldo(int edad, String nacionalidad){
         double sueldo=2500;
-
-
-        return 0;
+        double bono=0;
+        bono  = calcularBono(nacionalidad);
+        sueldo += bono; // sueldo = sueldo + bono;
+        if( edad >=15 && edad<=20){
+            sueldo+=1400;
+        } else if (edad>= 21 && edad<=25) {
+            sueldo+=1500;
+        } else if (edad>=26 && edad<=30) {
+            sueldo+=1200;
+        } else {
+            sueldo+=800;
+        }
+        return sueldo;
+    }
+    static double calcularBono(String nacionalidad){
+        double bono = 0;
+        if(nacionalidad.equals("E")){
+            bono = 500;
+        }
+        return bono;
     }
 
 }
