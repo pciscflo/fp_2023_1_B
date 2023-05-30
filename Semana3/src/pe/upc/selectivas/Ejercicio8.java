8dc9e760c274a1615d32b4763f01441238951bd0
professor <pciscflo@upc.edu.pe>
2023-05-29 21:02:33 -0500

    02.01

---
+++ Semana3/src/pe/upc/selectivas/Ejercicio8.java
@@ -0,0 +1,15 @@
+package pe.upc.selectivas;
+
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        System.out.println(calcularSueldo(26, "E"));
+    }
+    static double calcularSueldo(int edad, String nacionalidad){
+        double sueldo=2500;
+
+
+        return 0;
+    }
+
+}
+
