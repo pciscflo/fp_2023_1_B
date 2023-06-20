5a714590522d08be7970cf7c2464a7505756e5f5
professor <pciscflo@upc.edu.pe>
2023-06-19 21:16:02 -0500

    19.06.v4

--- Semana6/src/pe/upc/arreglos/Ejercicio3.java
+++ Semana6/src/pe/upc/arreglos/Ejercicio3.java
@@ -18,6 +18,32 @@ de entrada un arreglo con la cantidad de unidades vendidas por cada agente comer
 4) Si se sabe que por cada unidad vendida se tiene que pagar una comisión de ventas de
 300 soles, determinar cuánto es lo que se tiene que pagar como comisión este mes.
  */
+    static int[] totalUnidades = {3, 4, 6, 10, 2};
+    static String[] agentes = {"Mario", "Jorge", "Luis", "Carlos", "Carmen"};
 
-    
+
+    public static void main(String[] args) {
+        System.out.println("Agentes que superaron la cuota:");
+        String[] agentes = obtenerAgentesSuperaronCuota();
+        for(int i=0; i< agentes.length; i++)
+        {
+            if(agentes[i]!=null) {
+                System.out.println(agentes[i]);
+            }
+        }
+    }
+
+    static String[] obtenerAgentesSuperaronCuota(){
+        String[] nombres = new String[5];
+        int j = 0;
+        for(int i=0; i< totalUnidades.length; i++){
+            if(totalUnidades[i]>5){
+                nombres[j] = agentes[i];
+                j++;
+            }
+        }
+        return nombres;
+    }
+
+
 }
