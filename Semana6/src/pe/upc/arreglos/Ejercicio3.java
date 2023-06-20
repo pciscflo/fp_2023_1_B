339b9d64beb80272537cc4318383d723250bc100
professor <pciscflo@upc.edu.pe>
2023-06-19 21:02:30 -0500

    19.06.v3

---
+++ Semana6/src/pe/upc/arreglos/Ejercicio3.java
@@ -0,0 +1,23 @@
+package pe.upc.arreglos;
+
+public class Ejercicio3 {
+    /*
+Ejercicio: Ventas mensuales
+Una empresa vendedora de fotocopiadoras necesita calcular cual ha sido el volumen de
+ventas de cada uno de sus agentes comerciales durante el presente mes y en base a eso
+determinar distintos indicadores así como montos a pagar en comisión de ventas.
+
+Se le solicita:
+1) Obtener el total de unidades vendidas durante el presente mes.
+Para eso se tiene como dato de entrada un arreglo con la cantidad de unidades vendidas
+por cada agente comercial.
+2) Si se sabe que la cuota de venta de cada vendedor es 5 unidades, determinar cuál es el
+porcentaje de vendedores que superaron la cuota de venta. Para eso se tiene como dato
+de entrada un arreglo con la cantidad de unidades vendidas por cada agente comercial.
+3) Se desea saber quién(es) fueron los agentes de ventas que superaron la cuota de venta.
+4) Si se sabe que por cada unidad vendida se tiene que pagar una comisión de ventas de
+300 soles, determinar cuánto es lo que se tiene que pagar como comisión este mes.
+ */
+
+    
+}
