8a0b7b882adfd8257cac5892f646c4ca5c873269
professor <pciscflo@upc.edu.pe>
2023-06-26 21:12:49 -0500

    26.06.v4

---
+++ Semana7/src/pe/upc/cadenas/Ejercicio3.java
@@ -0,0 +1,48 @@
+package pe.upc.cadenas;
+
+public class Ejercicio3 {
+    /*
+    Una universidad ha organizado un concurso en donde participan los alumnos que coleccionen el
+    álbum Panono del Mundial Rusia 2018. El álbum Panono tiene 670 figuras y está distribuido de
+    la siguiente manera:
+
+-	18 figuras del tipo “especiales”
+-	12 figuras del tipo “estadio”
+-	32 figuras del tipo “escudos”
+-	608 figuras del tipo “jugadores”
+
+La universidad utiliza la siguiente codificación para organizar los datos de cada alumno participante:
+
+MO00100614520170013
+
+En donde:
+MO (2 caracteres): Sede donde se encuentra el alumno dueño del álbum.
+00 (2 caracteres): Cantidad de figuras del tipo "especiales" que tiene el álbum.
+10 (2 caracteres): Cantidad de figuras del tipo "estadios" que tiene el álbum.
+06 (2 caracteres): Cantidad de figuras del tipo "escudos" que tiene el álbum.
+145 (3 caracteres): Cantidad de figuras del tipo "jugadores" que tiene el álbum.
+20170013 (8 caracteres): Código del alumno
+
+Como dato de prueba se tiene los siguientes códigos de participantes:
+
+Códigos
+MO00100614520170013
+SI12011519220140012
+SM05122514520162573
+VI01061917120127351
+SM02053210220180312
+MO08041908820169815
+
+Usted, como integrante del equipo de la universidad, desarrollará un programa en donde:
+
+1.	Dado un código, indicar la cantidad de figuras del tipo “jugadores” que tiene álbum.
+2.	Dado un arreglo de códigos, identificar cuantos álbumes pertenecen a una sede determinada.
+3.	Dado un arreglo de códigos, identificar el código del alumno que tiene la mayor cantidad de figuras en una determinada sede.
+4.	Dado un arreglo de códigos, identificar cuantos álbumes tienen menos de 250 figuras en total.
+ */
+    
+
+    public static void main(String[] args) {
+
+    }
+}
